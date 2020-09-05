403fda272ea029e1806ec53b89c2decc7417761b
Erick <ericksepulveda94.com>
2020-09-05 13:32:43 -0400

    insert usuarios funcional

--- insertar_usuario.php
+++ insertar_usuario.php
@@ -1,10 +1,20 @@
 <?php
 include 'conexion.php';
-$usu_alias=$_POST['alias'];
-$usu_correo=$_POST['correo'];
-$usu_password=$_POST['password'];
 
-$consulta="insert into usuarios values('".$usu_alias."','".$usu_correo."','".$usu_password."')";
+if(isset($_POST['alias'],$_POST['correo'],$_POST['password']) and $_POST['alias']!="" and $_POST['correo']!="" and $_POST['password']!=""){
+ 
+    $usu_alias=$_POST['alias'];
+    $usu_correo=$_POST['correo'];
+    $usu_password=$_POST['password'];   
+}
+
+
+
+$consulta="insert into usuarios
+            (id, alias, correo, password)
+            VALUES
+            (DEFAULT, '".$usu_alias."','".$usu_correo."', '".$usu_password."')";
+
 mysqli_query($conexion,$consulta) or die ("Problemas al insertar".mysqli_error($conexion));
 mysqli_close($conexion);
 
